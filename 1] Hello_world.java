8a59f49c878d69d3306a4779337ae33c0266ed45
Ashish Bhosale <bhosaleashish835@gmail.com>
2026-09-19 00:26:09 +0530

    Create  1] Hello_world.java
    First Java Program

---
+++ 1] Hello_world.java
@@ -0,0 +1,9 @@
+package com.ashish; // add your own package , this is nothing but a file in your main folder.
+
+public class Main {
+   public static void main(String[] args) {
+       System.out.print("Hello World");
+       System.out.println("My name is Ashish");
+
+    }
+}
