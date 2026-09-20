6ce419a54fab68ce8db4ee1928c6b8c334665c54
Ashish Bhosale <bhosaleashish835@gmail.com>
2026-09-20 22:54:24 +0530

    Create Count_1_to_5.java
    count to 1 to 5 using if condition basic program

---
+++ Count_1_to_5.java
@@ -0,0 +1,11 @@
+package com.ashish;
+
+public class Basics {
+    public static void main(String[] args) {
+          int a = 10;
+          if (a == 10);{
+              System.out.println("Hello World");
+          }
+
+        }
+}
