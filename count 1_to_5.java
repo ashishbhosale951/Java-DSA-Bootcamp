f0db067cf543a8fbbcd8db401c0362a8c105f848
Ashish Bhosale <bhosaleashish835@gmail.com>
2026-09-20 22:57:46 +0530

    Create count 1_to_5.java
    count to 1 to 5 using while loop and for loop

---
+++ count 1_to_5.java
@@ -0,0 +1,18 @@
+package com.ashish;
+
+public class Basics {
+    public static void main(String[] args) {
+    //while loop
+        int count = 1;
+        while(count != 5) {
+           System.out.println(count);
+              count++;
+          }
+
+        // for Loop
+        for (int count = 1; count !=5; count++){
+            System.out.println(count);
+        }
+
+    }
+}
